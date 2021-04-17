813caf959fa55f0c2a4ca974d164e18fc16d1e78
PyTis <GitHub@PyTis.com>
2021-04-16 22:21:59 -0400

    wip

--- src/audio_reviewer/lib/project.py
+++ src/audio_reviewer/lib/project.py
@@ -1,5 +1,5 @@
 
-import os
+import os, sys
 import configobj as COBJ
 
 acceptable_extensions = ['.midi', '.m4a', '.mp3', '.wav', '.wma']
@@ -12,10 +12,21 @@ class Project(object):
 
   """
   _cfile=None
-  _name=None
-  _path=None
+  name = None
   path = None
   _folders = ['to-keep', 'to-review', 'to-remove', 'other']
+  _data = {
+    'to-keep' : {},
+    'to-review' : {},
+    'to-remove' : {},
+    'other' : {}
+  }
+
+  def set_data(self, data):
+    self._data = data
+  def get_data(self):
+    return self._data
+  data=property(get_data, set_data)
 
   def set_folders(self,folders):
     self._folders=folders
@@ -39,12 +50,6 @@ class Project(object):
   def other(self):
     return os.path.abspath(os.path.join(self.path, 'to-other'))
 
-  def set_name(self,name):
-    self._name=name
-  def get_name(self):
-    return self._name
-  name=property(get_name, set_name)
-
   def set_cfile(self,cfile):
     self._cfile=cfile
   def get_cfile(self):
@@ -63,12 +68,9 @@ class Project(object):
     
     if name: self.name=name
     if path: self.path=path
-    
-    self.cfile = COBJ.ConfigObj(self._config_file_path())
 
-    if self.cfile.get('name'): self.name=self.cfile.get('name')
-    if self.cfile.get('path'): self.path=self.cfile.get('path')
-    if self.cfile.get('folders'): self.folders=self.cfile.get('folders')
+    if self._config_file_path and os.path.exists(self._config_file_path):
+      self.load()
 
   def validateCoreFolders(self):
     call_save = False
@@ -89,39 +91,126 @@ class Project(object):
         self.saveData()
 
 
-
-
   # ------------------------------------------------------------------------- #
+  def addBookmark(self, SoundFile, bookmark={}):
+    btime = bookmark['time']
 
+    # is this soundfile already in the dict?
+    if not self.data.get(SoundFile.folder, {}):
+      self.data[SoundFile.folder] = {}
+
+    if not self.data[SoundFile.folder].get(SoundFile.fpath):
+      self.data[SoundFile.folder][SoundFile.fpath] = {}
+  
+    
+    self.data[SoundFile.folder][SoundFile.fpath].update( {
+      'name' : SoundFile.name,
+      'filename' : SoundFile.filename,
+      'filepath' : SoundFile.filepath,
+    })
+  
+    bookmarks=self.data[SoundFile.folder][SoundFile.fpath].get('bookmarks', {})
+
+    bookmarks[btime] = bookmark
+
+    self.data[SoundFile.folder][SoundFile.fpath]['bookmarks'] = bookmarks
+    self.save()
+
+  def bookmarksFor(self, SoundFile):
+    return self.data.get(SoundFile.folder, {}).get(SoundFile.fpath,
+      {}).get('bookmarks', {})
+
+  def removeBookmark(self, SoundFile, bookmark_time):
+    bookmarks = self.bookmarksFor(SoundFile)
+    del bookmarks[bookmark_time]
+    self.data[SoundFile.folder][SoundFile.fpath]['bookmarks'] = bookmarks
+    self.save()
+
+  def moveFile(self, SoundFile, old_path, old_folder, new_path, new_folder):
+  #self.Project.moveFile(old_path, old_folder, new_path, new_folder=target)
+    data = self.data.get(old_folder, {}).get(old_path, {})
+    bookmarks = data.get('bookmarks', {})
+
+    if data:
+      del self.data[old_folder][old_path]
+      data['filepath'] = new_path
+
+    data = {
+      'name' : SoundFile.name,
+      'filename' : SoundFile.filename,
+      'filepath' : new_path,
+      'bookmarks' : bookmarks
+    }
+
+    if not self.data.get(new_folder, {}):
+      self.data[new_folder] = {}
+
+    self.data[new_folder][new_path] = data
+    self.save()
+
+  @property
   def _config_file_path(self):
     if not self.path:
       return None
     return os.path.abspath(os.path.join(self.path, 'data.ini'))
 
+              
   def load(self):
-    self.cfile = COBJ.ConfigObj(self.path)
+    self.cfile = COBJ.ConfigObj(self._config_file_path)
+    data = {}
+
     for k, v  in self.cfile.items():
+      if k == 'data':
+        for kk, vv in v.items():
+          data[kk] = vv
+
       self.__setattr__(k, v)
+    """ 
+    for folder_name, folders in data.items():
+      for file_name, file_data in folders.items():
+        fixed_bookmarks = {}
 
-#    if self.cfile.get('name'): self.name=self.cfile.get('name')
-#    if self.cfile.get('path'): self.path=self.cfile.get('path')
-#    if self.cfile.get('folders'): self.folders=self.cfile.get('folders')
+        if data[folder_name][file_name].get('bookmarks'):
 
+          bookmarks = data[folder_name][file_name].get('bookmarks')
+          if bookmarks and type(bookmarks) is type([]):
+            for bookmark in bookmarks:
+              bookmark = eval(bookmark)
+              fixed_bookmarks[bookmark['time']] = {
+                  'time':bookmark['time'],
+                  'summ':bookmark['summ'],
+                  'desc':bookmark['desc']
+              }
+          elif bookmarks and type(bookmarks) is type({}):
+            fixed_bookmarks.update(bookmarks)
 
-  def save(self):
-    cfile = COBJ.load(self._config_file_path(), True)
-    print('save called')
-    cfile['name'] = self.name
-    print('name set to', self.name)
-    cfile['path'] = self.path
-    print('path set to', self.path)
-    cfile['folders'] = self.folders
-    print('about to call save')
+        data[folder_name][file_name]['bookmarks'] = fixed_bookmarks
+    """
+          
+    self.data = data
+    
+  @property
+  def record(self):
+    return {
+  #    'project-settings' : '',
+      'name'    : self.name,
+      'path'    : self.path,
+      'folders' : self.folders,
+      'data' : self.data
+    }
 
-    print(cfile)
-
+  def write(self):
+    cfile = COBJ.load(self._config_file_path, True)
+    cfile.update(self.record)
     cfile.save()
 
+  def save(self):
+    cfile = COBJ.load(self._config_file_path, True)
+    cfile['name'] = self.name
+    cfile['path'] = self.path
+    cfile['folders'] = self.folders
+    cfile['data'] = self.data
+    cfile.save()
 
   def create(self):
     # create config file for project
@@ -131,6 +220,8 @@ class Project(object):
     self.build()
 
     # save config file
+
+
 
   def removeFolder(self, name):
     if name.lower() in self._folders:
